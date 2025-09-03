b0649a2b2e9c434f2ddd1b73748a62e0a4ec9156
pablo peralta <pablofedericoperalta@gmail.com>
2025-09-03 11:53:39 -0300

    acij agregado

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 interface Project {
   id: number;
@@ -12,74 +12,93 @@ interface Project {
 const projectsData: Project[] = [
   {
     id: 1,
-    title: "Bibliotecas Populares Cba",
-    description: "Website that links all public libraries in the province of Córdoba, Argentina. Built with technologies such as JS, Express and Node.js. The database is MongoDB, other tools for authentication and geolocation.",
-    image: '/screenshots/bibliotecas-populares-cba.png',
-    liveLink: "https://www.bibliotecaspopularescba.com/",
-    codeLink: "https://github.com/pablo-peralta-cba/bibliotecas-populares-cba",
+    title: 'ACIJ + La Poderosa',
+    description:
+      'Fully frontend, visually appealing website developed using JavaScript and plain CSS. Made a complex social and economic study accessible to users through an effective scrollytelling approach.',
+    image: '/screenshots/acij-la-poderosa.png',
+    liveLink: 'https://lapoderosa.acij.org.ar/',
+    codeLink: 'https://github.com/Quimey927/acij',
   },
   {
     id: 2,
-    title: "E-commerce Product Page",
-    description: "This is a solution to Sneakers challenge on Frontend Mentor. Responsive e-commerce product page built using React/Typescript with Vite structure. For the styles I used a combination of Bootstrap classes and CSS.",
-    image: "/screenshots/ecommerce-page.png",
-    liveLink: "https://sneakers-ecommerce.onrender.com/",
-    codeLink: "https://github.com/pablo-peralta-cba/E-commerce-product-page",
+    title: 'Bibliotecas Populares Cba',
+    description:
+      'Website that links all public libraries in the province of Córdoba, Argentina. Built with technologies such as JS, Express and Node.js. The database is MongoDB, other tools for authentication and geolocation.',
+    image: '/screenshots/bibliotecas-populares-cba.png',
+    liveLink: 'https://www.bibliotecaspopularescba.com/',
+    codeLink: 'https://github.com/pablo-peralta-cba/bibliotecas-populares-cba',
   },
   {
     id: 3,
-    title: "AnotaAI  - Backend API",
-    description: "Robust and scalable RESTful API built using Node.js, Express and TypeScript. Backend solution for managing products and categories catalogs CRUD. Mongoose for interacting with MongoDB, plus AWS services like SQS and S3.",
-    image: '/screenshots/backend-api.png',
-    liveLink: "",
-    codeLink: "https://github.com/pablo-peralta-cba/pablo-peralta-anotaai-test-backend-nodejs",
+    title: 'E-commerce Product Page',
+    description:
+      'This is a solution to Sneakers challenge on Frontend Mentor. Responsive e-commerce product page built using React/Typescript with Vite structure. For the styles I used a combination of Bootstrap classes and CSS.',
+    image: '/screenshots/ecommerce-page.png',
+    liveLink: 'https://sneakers-ecommerce.onrender.com/',
+    codeLink: 'https://github.com/pablo-peralta-cba/E-commerce-product-page',
   },
   {
     id: 4,
-    title: "Online Calculator",
-    description: "Light project using Typescript and React. Really easy to use, this calculator was developed for the Front End Libraries certification on FreeCodeCamp.org",
-    image: '/screenshots/js-calculator.png',
-    liveLink: "https://js-calculator-eivv.onrender.com/",
-    codeLink: "https://github.com/pablo-peralta-cba/js-calculator",
+    title: 'AnotaAI  - Backend API',
+    description:
+      'Robust and scalable RESTful API built using Node.js, Express and TypeScript. Backend solution for managing products and categories catalogs CRUD. Mongoose for interacting with MongoDB, plus AWS services like SQS and S3.',
+    image: '/screenshots/backend-api.png',
+    liveLink: '',
+    codeLink:
+      'https://github.com/pablo-peralta-cba/pablo-peralta-anotaai-test-backend-nodejs',
   },
   {
     id: 5,
-    title: "Markdown Previewer",
-    description: "React app displaying an text editor and a preview area, where the user can see reflected all the markdown styles used. Developed for the Front End Libraries certification on FreeCodeCamp.org",
-    image: '/screenshots/markdown-previewer.png',
-    liveLink: "https://markdown-previewer-wp88.onrender.com/",
-    codeLink: "https://github.com/pablo-peralta-cba/markdown-previewer",
+    title: 'Online Calculator',
+    description:
+      'Light project using Typescript and React. Really easy to use, this calculator was developed for the Front End Libraries certification on FreeCodeCamp.org',
+    image: '/screenshots/js-calculator.png',
+    liveLink: 'https://js-calculator-eivv.onrender.com/',
+    codeLink: 'https://github.com/pablo-peralta-cba/js-calculator',
   },
   {
     id: 6,
-    title: "Random Quote Generator",
-    description: "This project displays a colorful quote generator. The app fetches phrases from diverse authors directly from an online API, using asynchronous functions. Created to obtain the Front End Libraries certification.",
-    image: '/screenshots/random-quote.png',
-    liveLink: "https://random-quote-generator-dt4o.onrender.com/",
-    codeLink: "https://github.com/pablo-peralta-cba/quote-machine-exercise",
+    title: 'Markdown Previewer',
+    description:
+      'React app displaying an text editor and a preview area, where the user can see reflected all the markdown styles used. Developed for the Front End Libraries certification on FreeCodeCamp.org',
+    image: '/screenshots/markdown-previewer.png',
+    liveLink: 'https://markdown-previewer-wp88.onrender.com/',
+    codeLink: 'https://github.com/pablo-peralta-cba/markdown-previewer',
   },
 ];
 
 const Projects: React.FC = () => {
   return (
-    <section id="projects" className="projects-section py-5">
-      <div className="container">
+    <section id='projects' className='projects-section py-5'>
+      <div className='container'>
         <h2>Projects</h2>
-        <div className="projects-grid">
+        <div className='projects-grid'>
           {projectsData.map((project) => (
-            <div key={project.id} className="project-cell">
-              <img src={project.image} alt={project.title} className="project-image" />
-              <div className="project-overlay">
+            <div key={project.id} className='project-cell'>
+              <img
+                src={project.image}
+                alt={project.title}
+                className='project-image'
+              />
+              <div className='project-overlay'>
                 <h3>{project.title}</h3>
                 <p>{project.description}</p>
-                <div className="project-links">
+                <div className='project-links'>
                   {project.liveLink && (
-                    <a href={project.liveLink} target="_blank" rel="noopener noreferrer">
+                    <a
+                      href={project.liveLink}
+                      target='_blank'
+                      rel='noopener noreferrer'
+                    >
                       Go to Website
                     </a>
                   )}
                   {project.codeLink && (
-                    <a href={project.codeLink} target="_blank" rel="noopener noreferrer">
+                    <a
+                      href={project.codeLink}
+                      target='_blank'
+                      rel='noopener noreferrer'
+                    >
                       View Code
                     </a>
                   )}
