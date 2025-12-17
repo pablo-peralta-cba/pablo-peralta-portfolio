51717d9f5fdbd69fdb62fb7e2d03b6754fb9602d
pablo peralta <pablofedericoperalta@gmail.com>
2025-12-17 17:50:37 -0300

    cambio color subtitulo y eliminé comentarios

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,15 +1,20 @@
-import React from "react";
-
+import React from 'react';
 
 const Home: React.FC = () => {
-
-    return(
-        <section id="home" className="home-section d-flex flex-column justify-content-center align-items-center text-center">
-<h1 className="home-title">Hi there. I'm <span className="brandName">Pablo</span></h1>
-<h3 className="home-subtitle">Fullstack Developer</h3>
-<p className="home-text">Robust and scalable web solutions</p>
-        </section>
-    )
-}
+  return (
+    <section
+      id='home'
+      className='home-section d-flex flex-column justify-content-center align-items-center text-center'
+    >
+      <h1 className='home-title'>
+        Hi there. I'm <span className='brandName'>Pablo</span>
+      </h1>
+      <h2 className='home-subtitle'>
+        Fullstack Developer - Frontend - Backend
+      </h2>
+      <p className='home-text'>Robust and scalable web solutions</p>
+    </section>
+  );
+};
 
 export default Home;
